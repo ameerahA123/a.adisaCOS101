a5458d77033bf12455e2fa35fb309fac343f3f8d
Adisa Ameerah Oladapo <ameerah.adisa@pau.edu.ng>
2024-10-31 22:46:00 +0100

    (My first commit)

---
+++ Week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!(); // Blank line
+
+    println!("hello ");
+
+    println!("format {} arguments", "some");
+
+    println!("My name is {}. I am the course rep of stream 1", "Dagogo William-Jumbo");
+}
